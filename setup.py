10b8056e470f220defa6d7c2d840a44a723592a3
woodpecker324 <woodpecker324@protonmail.com>
2019-04-12 20:49:25 +0200

    changed description

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
-    description="A wrapper around the `webbrowser` module of the standard library.",
+    description="A wrapper around the webbrowser module of the standard library.",
     install_requires=requirements,
     license="MIT license",
     long_description=readme + "\n\n" + history,
