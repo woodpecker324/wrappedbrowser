c60bac66224cf7b69ab4d4e3470a94502a84880c
woodpecker324 <woodpecker324@protonmail.com>
2019-04-13 15:38:26 +0200

    changed bumpversion config

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/woodpecker324/wrappedbrowser",
-    version="version='0.1.4'",
+    version="0.1.4",
     zip_safe=False,
 )
